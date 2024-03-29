ad6ab7baf88efb05e9855e2a656f05a644cc87d2
alemr214 <ale.mr0214@gmail.com>
2024-03-29 15:27:02 -0600

    GUI.py
    Modified GUI with new text, and colors and changed the state of the scroll bar of numbers and content

--- gui.py
+++ gui.py
@@ -3,9 +3,14 @@ from tkinter import ttk
 from tkinter import filedialog
 from lexer import lexer_action
 
+background = "#303446"
+foreground = "#d5cee3"
+font = "#232634"
+
 
 def on_content_changed(event=None):
     update_line_numbers()
+    sync_scrollbars()
 
 
 def update_line_numbers():
@@ -36,10 +41,13 @@ def lex_text():
 def display_tokens(token_list):
     new_window = tk.Toplevel(root)
     new_window.title("Tokens")
-    new_window.configure(bg="#282a36")
+    new_window.configure(bg=f"{background}")
     style = ttk.Style()
     style.configure(
-        "BW.TLabel", foreground="#f8f8f2", background="#282a36", font=("Helvetica", 13)
+        "BW.TLabel",
+        foreground=f"{foreground}",
+        background=f"{background}",
+        font=("Helvetica", 13),
     )
 
     tree = ttk.Treeview(
@@ -83,7 +91,7 @@ def exit_app():
 def show_error_popup(message):
     error_popup = tk.Toplevel(root)
     error_popup.title("Error")
-    error_popup.configure(bg="#282a36")
+    error_popup.configure(bg=f"{background}")
 
     window_width = 600
     window_height = 150
@@ -94,22 +102,30 @@ def show_error_popup(message):
     error_popup.geometry(f"{window_width}x{window_height}+{x_position}+{y_position}")
 
     error_label = tk.Label(
-        error_popup, text=message, bg="#282a36", fg="#f8f8f2", font=("Consolas", 12)
+        error_popup,
+        text=message,
+        bg=f"{background}",
+        fg=f"{foreground}",
+        font=("Consolas", 12),
     )
     error_label.pack(padx=10, pady=10)
     close_button = tk.Button(
         error_popup,
         text="De Acuerdo",
         command=error_popup.destroy,
-        bg="#6272a4",
-        fg="#f8f8f2",
+        bg=f"{background}",
+        fg=f"{foreground}",
         font=("Helvetica", 10),
     )
     close_button.pack(padx=10, pady=10)
 
 
+def sync_scrollbars():
+    line_number_box.yview_moveto(text_box.yview()[0])
+
+
 root = tk.Tk()
-root.title("LexLuaThor")
+root.title("Ruby Lexer")
 root.configure(bg="#282a36")
 
 window_width = 1280
@@ -120,7 +136,7 @@ x_position = (screen_width - window_width) // 2
 y_position = (screen_height - window_height) // 2
 root.geometry(f"{window_width}x{window_height}+{x_position}+{y_position}")
 
-line_number_frame = tk.Frame(root, width=50, bg="#44475a")
+line_number_frame = tk.Frame(root, width=50, bg=f"{background}")
 line_number_frame.pack(side=tk.LEFT, fill=tk.Y, padx=10, pady=10)
 
 line_number_box = tk.Text(
@@ -129,37 +145,38 @@ line_number_box = tk.Text(
     padx=4,
     pady=4,
     wrap=tk.NONE,
-    bg="#44475a",
-    # Match background color, text color, and font
-    fg="#f8f8f2",
+    bg=f"{background}",
+    fg=f"{foreground}",
     font=("Consolas", 13),
 )
 line_number_box.pack(side=tk.LEFT, fill=tk.Y)
 line_number_box.config(state="disabled")
 
-text_frame = tk.Frame(root, bg="#44475a")
+text_frame = tk.Frame(root, bg=f"{background}")
 text_frame.pack(side=tk.LEFT, padx=10, pady=10, fill=tk.BOTH, expand=True)
 
 text_box = tk.Text(
-    text_frame, height=40, width=100, bg="#44475a", fg="#f8f8f2", font=("Consolas", 13)
+    text_frame,
+    height=40,
+    width=100,
+    bg=f"{background}",
+    fg=f"{foreground}",
+    font=("Consolas", 13),
 )  # Set text box background color, text color, and font
 text_box.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
 text_box.bind("<Key>", on_content_changed)
 
-
-button_frame = tk.Frame(root, bg="#282a36")
+button_frame = tk.Frame(root, bg=f"{background}")
 button_frame.pack(side=tk.TOP, padx=10, pady=10)
 
 lex_button = tk.Button(
     button_frame,
-    text="Lex it!",
+    text="Lexical Analysis",
     command=lex_text,
     width=10,
     height=2,
-    # Green button with white text
-    bg="#50fa7b",
-    fg="#44475a",
+    fg="green",
     bd=0,
     font=("Helvetica", 12, "bold"),
 )
@@ -167,13 +184,10 @@ lex_button.pack(side=tk.TOP, padx=5, pady=5)
 
 import_button = tk.Button(
     button_frame,
-    text="Importar\nArchivo",
+    text="Import\nFile",
     command=import_file,
     width=10,
     height=2,
-    # Blue button with white text
-    bg="#bd93f9",
-    fg="#44475a",
     bd=0,
     font=("Helvetica", 12, "bold"),
 )
@@ -181,13 +195,11 @@ import_button.pack(side=tk.TOP, padx=5, pady=5)
 
 clear_button = tk.Button(
     button_frame,
-    text="Borrar\nContenido",
+    text="Cleanup",
     command=clear_content,
     width=10,
     height=2,
-    # Yellow button with white text
-    bg="#ffb86c",
-    fg="#44475a",
+    fg="#ffaa2c",
     bd=0,
     font=("Helvetica", 12, "bold"),
 )
@@ -195,13 +207,11 @@ clear_button.pack(side=tk.TOP, padx=5, pady=5)
 
 exit_button = tk.Button(
     root,
-    text="Salir",
+    text="Exit!",
     command=exit_app,
     width=10,
     height=2,
-    bg="#ff5555",
-    # Red button with white text
-    fg="white",
+    fg="#ff5555",
     bd=0,
     font=("Helvetica", 12, "bold"),
 )
